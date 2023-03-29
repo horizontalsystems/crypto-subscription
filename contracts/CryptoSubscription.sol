6af25100f25e1e1c6d1fff523db6f87ca808e7e2
Ermat <ealymbaev@gmail.com>
2023-03-29 14:59:42 +0600

    Add constructor parameters and storage for them
    - add `token`, `commissionRate`, `discountRate` and `plans` fields

--- contracts/CryptoSubscription.sol
+++ contracts/CryptoSubscription.sol
@@ -1,10 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract CryptoSubscription {
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-    function zeroAddress() public pure returns (address) {
-        return address(0);
+contract CryptoSubscription is AccessControl {
+    IERC20Metadata private _token;
+    uint16 public commissionRate;
+    uint16 public discountRate;
+
+    mapping(uint16 => uint16) private _plans;
+
+    constructor(address _tokenAddress, uint16 _commissionRate, uint16 _discountRate, uint16[] memory _planDurations, uint16[] memory _planCosts) {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _token = IERC20Metadata(_tokenAddress);
+        commissionRate = _commissionRate;
+        discountRate = _discountRate;
+
+        uint planLength = _planDurations.length;
+        for (uint i = 0; i < planLength; i++) {
+            _plans[_planDurations[i]] = _planCosts[i];
+        }
+    }
+
+    function tokenAddress() public view returns (address) {
+        return address(_token);
+    }
+
+    function planCost(uint16 duration) public view returns (uint16) {
+        return _plans[duration];
     }
 
 }
--- test/CryptoSubscription.spec.ts
+++ test/CryptoSubscription.spec.ts
@@ -1,13 +1,22 @@
 import { expect, use } from 'chai'
 import { ethers } from 'hardhat'
-import { smock } from '@defi-wonderland/smock'
+import { FakeContract, smock } from '@defi-wonderland/smock'
 import { Wallet } from 'ethers'
-import { CryptoSubscription } from '../typechain-types'
+import { CryptoSubscription, IERC20Metadata } from '../typechain-types'
 
 use(smock.matchers)
 
 describe('CryptoSubscription', function () {
   let contract: CryptoSubscription
+  let token: FakeContract<IERC20Metadata>
+  let decimals = 6
+
+  let rateMultiplier = 1000
+
+  let commissionRate = 0.025
+  let discountRate = 0.015
+
+  let plans: { [key: number]: number } = { 30: 200, 90: 500, 180: 800 }
 
   let owner: Wallet
   let moderator: Wallet
@@ -16,13 +25,41 @@ describe('CryptoSubscription', function () {
   beforeEach(async () => {
     ;[owner, moderator, other] = await (ethers as any).getSigners()
 
+    token = await smock.fake('IERC20Metadata')
+    token.decimals.whenCalledWith().returns(decimals)
+
     const CryptoSubscription = await ethers.getContractFactory('CryptoSubscription', { signer: owner })
-    contract = await CryptoSubscription.deploy()
+    contract = await CryptoSubscription.deploy(
+      token.address,
+      commissionRate * rateMultiplier,
+      discountRate * rateMultiplier,
+      Object.keys(plans),
+      Object.values(plans)
+    )
   })
 
   describe('#constructor', () => {
-    it('tests something', async () => {
-      expect(await contract.zeroAddress()).to.eq('0x0000000000000000000000000000000000000000')
+    it('grants owner default admin role', async () => {
+      let defaultAdminRole = await contract.DEFAULT_ADMIN_ROLE()
+      expect(await contract.hasRole(defaultAdminRole, owner.address)).to.be.true
+    })
+
+    it('sets token to provided one', async () => {
+      expect(await contract.tokenAddress()).to.eq(token.address)
+    })
+
+    it('sets commission rate to provided one', async () => {
+      expect(await contract.commissionRate()).to.eq(commissionRate * rateMultiplier)
+    })
+
+    it('sets discount rate to provided one', async () => {
+      expect(await contract.discountRate()).to.eq(discountRate * rateMultiplier)
+    })
+
+    it('sets plans to provided ones', async () => {
+      for (let duration in plans) {
+        expect(await contract.planCost(duration)).to.eq(plans[duration])
+      }
     })
   })
 })
