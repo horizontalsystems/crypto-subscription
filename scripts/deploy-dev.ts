82bf497252a60acc11431bf94907abd35fe0387b
Ermat <ealymbaev@gmail.com>
2023-04-03 15:53:41 +0600

    Configure deploy scripts

--- scripts/deploy-dev.ts
+++ scripts/deploy-dev.ts
@@ -4,8 +4,24 @@ async function main() {
   const [deployer] = await ethers.getSigners()
   console.log('Deployer address:', deployer.address)
 
+  const tokenDecimals = 6
+  const commissionRate = 0.2
+  const discountRate = 0.1
+  const plans = { 30: 200, 90: 500, 180: 800 }
+
+  const Token = await ethers.getContractFactory('TestErc20')
+  const token = await Token.deploy(tokenDecimals)
+  await token.deployed()
+  console.log('Token address:', token.address)
+
   const CryptoSubscription = await ethers.getContractFactory('CryptoSubscription')
-  const contract = await CryptoSubscription.deploy()
+  const contract = await CryptoSubscription.deploy(
+    token.address,
+    commissionRate * 1000,
+    discountRate * 1000,
+    Object.keys(plans),
+    Object.values(plans)
+  )
   await contract.deployed()
   console.log('CryptoSubscription contract address:', contract.address)
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,11 +3,26 @@ import { ethers } from 'hardhat'
 async function main() {
   const [deployer] = await ethers.getSigners()
 
+  const tokenAddress = process.env.TOKEN_ADDRESS
+  const commissionRate = 0.2
+  const discountRate = 0.1
+  const plans = { 30: 200, 90: 500, 180: 800 }
+
+  if (tokenAddress === undefined) {
+    throw new Error('Token address is required')
+  }
+
   console.log('Deploying contract with the account:', deployer.address)
   console.log('Account balance:', (await deployer.getBalance()).toString())
 
   const CryptoSubscription = await ethers.getContractFactory('CryptoSubscription')
-  const contract = await CryptoSubscription.deploy()
+  const contract = await CryptoSubscription.deploy(
+    tokenAddress,
+    commissionRate * 1000,
+    discountRate * 1000,
+    Object.keys(plans),
+    Object.values(plans)
+  )
   await contract.deployed()
 
   console.log('CryptoSubscription contract address:', contract.address)
