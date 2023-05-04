a3fcc3d89fef6fb62cf2f9f7fe4b08bcd19204f1
Ermat <ealymbaev@gmail.com>
2023-05-04 12:26:01 +0600

    Add promo code creation to development deploy script

--- scripts/deploy-dev.ts
+++ scripts/deploy-dev.ts
@@ -1,8 +1,11 @@
 import { ethers } from 'hardhat'
 
 async function main() {
-  const [deployer] = await ethers.getSigners()
+  const [deployer, moderator, promoter] = await ethers.getSigners()
+
   console.log('Deployer address:', deployer.address)
+  console.log('Moderator address:', moderator.address)
+  console.log('Promoter address:', promoter.address)
 
   const tokenDecimals = 6
   const plans = { 30: 200, 90: 500, 180: 800 }
@@ -10,6 +13,7 @@ async function main() {
   const Token = await ethers.getContractFactory('TestErc20')
   const token = await Token.deploy(tokenDecimals)
   await token.deployed()
+
   console.log('Token address:', token.address)
 
   const CryptoSubscription = await ethers.getContractFactory('CryptoSubscription')
@@ -19,7 +23,18 @@ async function main() {
     Object.values(plans)
   )
   await contract.deployed()
+
   console.log('CryptoSubscription contract address:', contract.address)
+
+  await contract.connect(deployer).grantRole(await contract.MODERATOR_ROLE(), moderator.address)
+
+  const promoCodeName = 'Unstoppable'
+  const commissionRate = 0.2
+  const discountRate = 0.1
+  const duration = 30
+  await contract.connect(moderator).setPromoCode(promoter.address, promoCodeName, commissionRate * 1000, discountRate * 1000, duration)
+
+  console.log('Promo code created:', promoCodeName)
 }
 
 main()
